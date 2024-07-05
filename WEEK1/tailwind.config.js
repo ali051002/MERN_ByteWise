6af1301bab54c42960872f90c3cd43e3fc5f30e9
Ali Sufyan <malikali051002@gmail.com>
2024-07-05 11:50:32 +0500

    updated

---
+++ WEEK1/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['"Nunito Sans"', 'sans-serif'],
+      'serif': ['"Playfair Display"', 'serif'],
+      'mono': ['"Courier New"', 'monospace'],
+      'body' : ['Raleway']
+    }
+  },
+  plugins: [],
+}
+
